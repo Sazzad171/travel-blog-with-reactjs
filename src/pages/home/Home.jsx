9f1388796c1196f6fc8b110be6bae010906835e5
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-06-18 11:30:48 +0600

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,26 +12,44 @@ export default function Home() {
       <Container fluid className='px-0'>
         <Carousel>
           <Carousel.Item>
-            <img className="d-block w-100" src={banner1} alt="First slide" />
+            <div className="img-wrapper position-relative">
+              <img className="d-block w-100" src={banner1} alt="First slide" />
+              <div className="overlay"></div>
+            </div>
             <Carousel.Caption>
-              <h3 className='text-white'>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h1 className='text-white mb-30'>Travel To The Beautiful Place</h1>
+              <p className='mb-40'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                <br /> Lorem Ipsum has been the industry's standard .
+              </p>
+              <a href="#fsa" className='btn'>View Places</a>
             </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item>
-            <img className="d-block w-100" src={banner2} alt="Second slide" />
+            <div className="img-wrapper position-relative">
+              <img className="d-block w-100" src={banner2} alt="Second slide" />
+              <div className="overlay"></div>
+            </div>
             <Carousel.Caption>
-              <h3 className='text-white'>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h1 className='text-white mb-30'>Welcome To The Travel</h1>
+              <p className='mb-40'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                <br /> Lorem Ipsum has been the industry's standard .
+              </p>
+              <a href="#fsa" className='btn'>View Places</a>
             </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item>
-            <img className="d-block w-100" src={banner3} alt="Second slide" />
+            <div className="img-wrapper position-relative">
+              <img className="d-block w-100" src={banner3} alt="Second slide" />
+              <div className="overlay"></div>
+            </div>
             <Carousel.Caption>
-              <h3 className='text-white'>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h1 className='text-white mb-30'>Choose The Best Tour Package</h1>
+              <p className='mb-40'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                <br /> Lorem Ipsum has been the industry's standard .
+              </p>
+              <a href="#fsa" className='btn'>View Places</a>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>

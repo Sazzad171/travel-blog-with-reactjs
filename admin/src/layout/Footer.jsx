3cd131ac4b0985672d1bd714c16e06226281bedc
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-06-27 16:34:11 +0600

    update

---
+++ admin/src/layout/Footer.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Col, Container, Row} from 'react-bootstrap';
+
+// images
+import logo from '../assets/img/logo.png';
+
+export default function Footer() {
+  return (
+    <>
+      {/* footer top start */}
+      <footer className='footer-area bg-dark pt-60 pb-55'>
+        <Container>
+          <Row>
+            <Col md={4} className='mb-3'>
+              <div className="logo-area">
+                <Link to="/">
+                  <img src={logo} alt="logo" className='mb-3 w-75' />
+                </Link>
+                <p className="text-white">Nature lovers Bangladesh is a Bangladeshi Travel Blog.</p>
+              </div>
+            </Col>
+            <Col md={3} className='mb-3'>
+              <div className="links-area">
+                <h4 className="text-white mb-2 mb-md-3">Quick Links</h4>
+                <ul>
+                  <li className='mb-1'>
+                    <Link to="/" className='text-ash'>Home</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/contact" className='text-ash'>Contact US</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Terms & conditions</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Privacy & Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col md={3} className='mb-3'>
+              <div className="links-area">
+                <h4 className="text-white mb-2 mb-md-3">About Us</h4>
+                <ul>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Our story</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Working with us</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Tour guide</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Be our partner</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col md={2} className='mb-3'>
+              <div className="links-area">
+                <h4 className="text-white mb-2 mb-md-3">Support</h4>
+                <ul>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Customer Support</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Terms & conditions</Link>
+                  </li>
+                  <li className='mb-1'>
+                    <Link to="/coming-soon" className='text-ash'>Privacy & Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+
+      {/* footer bottom start */}
+      <div className="footer-bottom py-3">
+        <Container>
+          <p className="text-center text-white">&copy; { new Date().getFullYear() } - All rights reserved by Nature Lovers BD.</p>
+        </Container>
+      </div>
+    </>
+  )
+}

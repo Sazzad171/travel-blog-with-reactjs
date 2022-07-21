71aabebe33319841b6ade84c710b15c3ceea9301
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-07-21 18:54:32 +0600

    update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,90 +1,16 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 
 // components
 import PageHeader from '../../layout/PageHeader';
-
-// icons
-import { FaFacebookF, FaTwitter, FaYoutube, FaRegEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
+import GetInTouch from './GetInTouch';
+import ContactInfo from './ContactInfo';
 
 export default function Contact() {
   return (
     <>
       <PageHeader />
-      
-      {/* get in touch start */}
-      <section className="get-in-touch-area pt-60 pb-60">
-        <Container>
-          <Row className='align-items-center'>
-            <Col md={7} className='mb-3'>
-              <div className="details-area mb-3">
-                <h6 className="text-orange text-uppercase">Get in touch</h6>
-                <h2 className='text-uppercase'>REACH & CONTACT US!</h2>
-                <p className="text-ash">
-                  Fusce hic augue velit wisi quibusdam pariatur, iusto primis, nec nemo, rutrum. Vestibulum cumque laudantium. 
-                  Sit ornare mollitia tenetur, aptent. Eget feugiat error necessitatibus taciti..
-                </p>
-              </div>
-              <div className="social-icon-area">
-                <ul>
-                  <li>
-                    <a href="#f"><FaFacebookF /></a>
-                  </li>
-                  <li>
-                    <a href="#d"><FaTwitter /></a>
-                  </li>
-                  <li>
-                    <a href="#d"><FaYoutube /></a>
-                  </li>
-                </ul>
-              </div>
-            </Col>
-            <Col md={5}>
-              {/* map */}
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.82239289454!2d90.27923794728072!3d23.780887455957277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1656214879218!5m2!1sen!2sbd" title="map" className='rounded' width="100%" height="350" loading="lazy"></iframe>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      {/* get in touch end */}
-
-      {/* contact medium area start */}
-      <section className="contact-medium-area pt-60 pb-60">
-        <Container>
-          <Row>
-            <Col md={4} className='mb-3'>
-              <div className="item h-100 px-4 py-4 py-md-5 text-center">
-                <span className="icon mb-4"><FaRegEnvelope /></span>
-                <h4 className='text-black text-uppercase'>email address</h4>
-                <ul>
-                  <li>support@gmail.com</li>
-                  <li>info@domain.com</li>
-                </ul>
-              </div>
-            </Col>
-            <Col md={4} className='mb-3'>
-              <div className="item h-100 px-4 py-4 py-md-5 text-center">
-                <span className="icon mb-4"><FaPhoneAlt /></span>
-                <h4 className='text-black text-uppercase'>PHONE NUMBER</h4>
-                <ul>
-                  <li>+880 165367444</li>
-                  <li>+880 165367445</li>
-                </ul>
-              </div>
-            </Col>
-            <Col md={4} className='mb-3'>
-              <div className="item h-100 px-4 py-4 py-md-5 text-center">
-                <span className="icon mb-4"><FaMapMarkerAlt /></span>
-                <h4 className='text-black text-uppercase'>ADDRESS LOCATION</h4>
-                <ul>
-                  <li>Dhaka, Bangladesh</li>
-                </ul>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      {/* contact medium area end */}
+      <GetInTouch />
+      <ContactInfo />
     </>
   )
 }
---
+++ src/pages/place-details/PlaceRight.jsx
@@ -0,0 +1,153 @@
+import React, { useEffect } from 'react';
+import { Link, useParams } from "react-router-dom";
+import {Col, Container, Row, Card} from 'react-bootstrap';
+
+// components
+import PageHeader from '../../layout/PageHeader';
+
+// images
+import destination from '../../assets/img/destination.jpg';
+
+// icons
+import { BsArrowRightShort } from 'react-icons/bs';
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+// slick slider 
+import Slider from "react-slick";
+import "../../assets/plugins/slick/slick.css"; 
+import "../../assets/plugins/slick/slick.theme.css";
+import { useState } from 'react';
+
+export default function PlaceDetails({ places, gallery }) {
+
+  // state
+  const [placeDetails, setPlaceDetails] = useState({});
+
+  // slick slider
+  const slickSettings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
+
+  // get id
+  const params = useParams();
+
+  useEffect(() => {
+    places.map((place) => {
+      if(place.id === params.id) {
+        return (setPlaceDetails(place));
+      }
+      else return '';
+    });
+  }, [params, places]);
+
+  return (
+    <>
+      <PageHeader />
+
+      {/* page header */}
+      <section className='page-header-area'>
+        <Container fluid className='px-0'>
+          <div className="img-wrapper position-relative">
+            <img src={destination} alt="page header" />
+            <div className="heading-text position-absolute d-flex w-100 h-100 align-items-center justify-content-center">
+              <h2 className='text-white pt-5'>{ placeDetails.name }</h2>
+            </div>
+          </div>
+        </Container>
+      </section>
+      
+      {/* place details start */}
+      <section className="place-details-area pt-40 pb-40">
+        <Container>
+          <Row>
+            {/* left details */}
+            <Col md={8} className='mb-3'>
+              <div className="place-details-box">
+                <img src={ placeDetails.img } className='mb-3 img-fluid w-100' alt="place" />
+                <h3 className='mb-1 text-red'>{ placeDetails.name }</h3>
+                <p className="text-ash mb-3">
+                  <FaMapMarkerAlt className='text-orange me-1' /> { placeDetails.location }
+                </p>
+                <p>
+                   { placeDetails.description }
+                </p>
+              </div>
+            </Col>
+            {/* right others */}
+            <Col md={4}>
+              <div className="travel-tips-box p-4 mb-4 mb-md-5">
+                <h4 className='text-center text-white'>Travel Tips</h4>
+                <ul className='ms-3'>
+                  <li>Do your research</li>
+                  <li>Don’t draw attention</li>
+                  <li>Make copies of important documents</li>
+                  <li>Keep your friends and family updated</li>
+                  <li>Be wary of public Wi-Fi</li>
+                  <li>Safeguard your hotel room</li>
+                  <li>Be aware of your surroundings</li>
+                </ul>
+              </div>
+              <div className="img-slider">
+                <h4 className="text-center mb-3">Destination Images</h4>
+                <Slider {...slickSettings}>
+                  {gallery && gallery.map((galleryItem) => (
+                    <div className="slider-item" key={ galleryItem.id }>
+                      <img src={ galleryItem.img } alt="slider img" />
+                    </div>
+                  ))}
+                </Slider>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* place details end */}
+
+      {/* related places start */}
+      <section className="tour-places-area pt-40 pb-40">
+        <Container>
+          {/* section heading */}
+          <Row className='justify-content-center'>
+            <Col md={7}>
+              <h5 className="text-center text-orange mb-3">Related Destinations</h5>
+              <h2 className='text-center mb-4 mb-md-5'>Travel Most Popular Place In <br /> The World</h2>
+            </Col>
+          </Row>
+          
+          {/* place items */}
+          <Row>
+
+            {/* destination item */}
+            {places && places.slice(0, 3).map((place) => (
+              <Col md={4} className='mb-3' key={ place.id }>
+                <Card className='h-100 shadow border-0'>
+                  <Card.Img variant="top" src={ place.img } />
+                  <Card.Body>
+                    <Row className='align-items-center'>
+                      <Col xs={9}>
+                        <h4 className='mb-1'>{ place.name }</h4>
+                        <p className='text-ash'>{ place.location }</p>
+                      </Col>
+                      <Col xs={3}>
+                        <Link to={ `/place-details/${place.id}` } className="btn rounded-circle"><BsArrowRightShort /></Link>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+            
+          </Row>
+        </Container>
+      </section>
+      {/* related places end */}
+    </>
+  )
+}
